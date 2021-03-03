4a70a9af7773c86d3699867251edc352310997a1
ilsame9970 <ilsame9970@gmail.com>
2021-03-03 20:28:15 +0900

    2021-03-03 springboot security jwt start

---
+++ loginSecurity/src/main/java/com/lu/LoginSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSecurityApplication.class, args);
+	}
+
+}
